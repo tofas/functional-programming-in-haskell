99a71bef57d8b9489804e51c654d3edc92645851
Daniel Morales <daniel.morales@edreamsodigeo.com>
2019-02-06 18:32:47 +0100

    Created two parsers

--- todo-list/Main.hs
+++ todo-list/Main.hs
@@ -1,6 +1,29 @@
 module Main(main) where
 
+-- File watcher: stack build --fast --file-watch --exec todo-list
+
 import Options.Applicative
 
+type ItemIndex = Int
+
+defaultDataPath :: FilePath
+defaultDataPath = "~/stack.yaml"
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $
+    value defaultDataPath
+    <> long "data-path"
+    <> short 'p'
+    <> metavar "DATAPATH"
+    <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+
 main :: IO()
-main = putStrLn "Hello World"
+main = do
+    dataPath <- execParser (info (dataPathParser) (progDesc "Todo-list manager"))
+    putStrLn $ "dataPath=" ++ show dataPath
+
+    --itemIndex <- execParser (info (itemIndexParser) (progDesc "To-do list manager"))
+    --putStrLn $ "itemIndex=" ++ show itemIndex
